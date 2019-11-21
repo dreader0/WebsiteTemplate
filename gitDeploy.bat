fd863679d5fd4df71ef6a666105d5181f742879b
Mackenzie Quigley <mquigl01@uoguelph.ca>
2019-11-21 18:15:00 -0500

    Adding updates to git

---
+++ gitDeploy.bat
@@ -0,0 +1,4 @@
+"C:\Program Files\Git\bin\sh.exe" --login -i -c "git add ."
+"C:\Program Files\Git\bin\sh.exe" --login -i -c "git commit -m \"Adding updates to git\""
+"C:\Program Files\Git\bin\sh.exe" --login -i -c "git push"
+pause
